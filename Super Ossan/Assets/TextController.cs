447a8e7d0e17049be4d524dbe5c443c56cccb2d9
Paul Barré <paul.yvon.barre@gmail.com>
2018-03-20 09:03:58 +0900

    Started the screen for skills config

--- Super Ossan/Assets/TextController.cs
+++ Super Ossan/Assets/TextController.cs
@@ -7,15 +7,34 @@ public class TextController : MonoBehaviour {
 
 	public Text text;
 
-	// Use this for initialization
-	void Start ()
-	{
-		
+	private States myState;
+	private SkillSelection skillSelection;
+	private int money;
+	private int alcohoolLevel;
+
+	private enum States {
+		character_design
+	}
+
+	private enum SkillSelection {
+		money, alcohool
+	}
+
+	void Start () {
+		myState = States.character_design;
+		skillSelection = SkillSelection.money;
+		money = 1000;
 	}
 	
-	// Update is called once per frame
-	void Update () 
-	{
-		text.text = "Salut !";	
+	void Update () {
+		if 		(myState == States.character_design) 		{character_design();}
 	}
+
+	#region State handler methods
+	void character_design () {
+		text.text = "Argent: " + money + "\n" +
+					"Alcohool level: " + skillSelection + "\n\n" +
+					"Use Up arrow and Down arrow to select skill you want to customize. Press Right arrow to increase your skill, Left arrow to decrease your skill.";
+	}
+	#endregion
 }
