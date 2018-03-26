64454a6e56c95def589bae5e473a7ec154306e78
Paul Barré <paul.yvon.barre@gmail.com>
2018-03-26 12:59:58 +0900

    created scene to find chocolate into the trash from the street

--- Super Ossan/Assets/TextController.cs
+++ Super Ossan/Assets/TextController.cs
@@ -8,33 +8,43 @@ public class TextController : MonoBehaviour {
 	public Text text;
 
 	private States myState;
-	private SkillSelection skillSelection;
-	private int money;
-	private int alcohoolLevel;
 
 	private enum States {
-		character_design
-	}
-
-	private enum SkillSelection {
-		money, alcohool
+		street, trash, club
 	}
 
 	void Start () {
-		myState = States.character_design;
-		skillSelection = SkillSelection.money;
-		money = 1000;
+		myState = States.street;
 	}
 	
 	void Update () {
-		if 		(myState == States.character_design) 		{character_design();}
+		if 		(myState == States.street) 		{ street(); }
+		else if (myState == States.trash) 		{ trash(); }
+		else if (myState == States.club) 		{ club(); }
 	}
 
 	#region State handler methods
-	void character_design () {
-		text.text = "Argent: " + money + "\n" +
-					"Alcohool level: " + skillSelection + "\n\n" +
-					"Use Up arrow and Down arrow to select skill you want to customize. Press Right arrow to increase your skill, Left arrow to decrease your skill.";
+	void street ()
+	{
+		text.text = "You are in one of those noisy streets of Kabukicho, the pocket full " +
+					"enough of money to go to the Friday Princess club.\n" +
+					"You'll finally be able to meet your favourite hostess Momo-chan.\n\n" +
+					"Press C to enter the Club, or T to have a look to the trash to which a drunk man is talking to.";
+		if 		(Input.GetKeyDown (KeyCode.C)) { myState = States.club; }
+		else if (Input.GetKeyDown (KeyCode.T)) { myState = States.trash; }
+	}
+
+	void trash () {
+		text.text = "A drunk man is talking to the trash. You just ignore him and dive a hand inside " +
+					"the trash...\n" +
+					"Oh! It seems you got something! You proudly taking out the trash a brand new \n" +
+					"box of chocolate. They look new and the box does not smeel bad. Maybe Momo-chan would be \n" +
+					"happy to get them!\n\n" +
+					"Press S to go back to the Street.";
+		if 		(Input.GetKeyDown (KeyCode.S)) { myState = States.street; }
+	}
+
+	void club () {
 	}
 	#endregion
 }
