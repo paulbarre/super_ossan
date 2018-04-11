0ab588e923e3247fd806a62fc9b423d5fc5e57d7
Paul Barré <paul.yvon.barre@gmail.com>
2018-04-11 17:33:03 +0900

    Split stages in different scripts

---
+++ Super Ossan/Assets/Scripts/StageManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StageManager : MonoBehaviour {
+
+	public Text text;
+	public Text[] options;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SetText (string text) {
+		this.text.text = text;
+	}
+
+	public void SetOptions (string[] options) {
+		for (int i = 0; i < options.Length; i++) {
+			this.options[i].text = options[i];
+		}
+	}
+}
---
+++ Super Ossan/Assets/Scripts/StreetStage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StreetStage : MonoBehaviour {
+
+	StageManager stageManager;
+
+	// Use this for initialization
+	void Start () {
+		stageManager = GameObject.FindObjectOfType<StageManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		stageManager.SetText("You are in one of those noisy streets of Kabukicho, the pocket full " +
+					"enough of money to go to the Friday Princess club.\n" +
+					"You'll finally be able to meet your favourite hostess Momo-chan.\n\n" +
+					"Press C to enter the Club, or T to have a look to the trash to which a drunk man is talking to.");
+	}
+}
