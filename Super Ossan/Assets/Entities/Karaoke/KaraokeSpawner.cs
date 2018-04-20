93f3fdab691d366d1001581edc8ccf421a6d28e6
Paul Barré <paul.yvon.barre@gmail.com>
2018-04-20 13:07:46 +0900

    Instantiate buttons and shred them when they leave the screen

---
+++ Super Ossan/Assets/Entities/Karaoke/KaraokeSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KaraokeSpawner : MonoBehaviour {
+
+	public GameObject button;
+	public float spawningRate = 0.5f;
+
+	void Start () {
+		InvokeRepeating ("InstantiateButtons", 0, spawningRate);
+	}
+
+	void InstantiateButtons () {
+		foreach (Transform child in transform) {
+			InstantiateButton (child);
+		}
+	}
+
+	void InstantiateButton (Transform parent) {
+		Instantiate (button, parent);
+	}
+}
